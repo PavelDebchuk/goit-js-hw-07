e23ffc955a2d3c8c2f81bf45123bae8e1a80fe47
Pavel <Pavel.Debchuk@gmail.com>
2021-11-16 22:42:23 +0300

    дз
    перша частина

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,6 +1,4 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
-//1. Создание и рендер разметки по массиву данных galleryItems и предоставленному шаблону элемента галереи.
 
 const galeryContainer = document.querySelector('.gallery');
 const cardGalery = createGalery('galery');
@@ -20,30 +18,29 @@ function createGalery (galery) {
   return markup;
 }
 
-//2. Реализация делегирования на div.gallery и получение url большого изображения.
-const imgBig = document.querySelector('.gallery__link').href;
-console.log(imgBig);
-//забороняємо переходити по посиланні
-
 const folsA = document.querySelector('.gallery');
 folsA.addEventListener("click", noLink);
 
-function noLink(events) {
-  events.preventDefault();
+  function noLink(events) {
+    events.preventDefault();
   };
 
-//3. Подключение скрипта и стилей библиотеки модального окна basicLightbox.
-//Используй CDN сервис jsdelivr и добавь в проект ссылки на минифицированные (.min) файлы библиотеки.
-// 4. Открытие модального окна по клику на элементе галереи. Для этого ознакомься с документацией и примерами.
+
+let imgData;
+const imgBig = document.querySelector('.gallery');
+imgBig.addEventListener('click', imgOpen);
+  function imgOpen (event) {
+  return imgData = event.target.getAttribute('data-source');
+  }
+
+
 const imgModal = document.querySelector('.gallery');
 imgModal.addEventListener('click', modalOpen);
 
 function modalOpen(events) {
   const instance = basicLightbox.create(
-
- `<img src="${imgBig}">`
-
-  )
+ `<img src="${imgData}">`
+)
 instance.show()
 }
 
@@ -53,4 +50,3 @@ instance.show()
 
 
 
-//5. Замена значения атрибута src элемента <img> в модальном окне перед открытием. Используй готовую разметку модального окна с изображением из примеров библиотеки basicLightbox.
